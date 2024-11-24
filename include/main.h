8cbced1049f0d21d8054271aa1fbefde24ce1227
Kristófer Fannar Björnsson <kristoferfannarb@gmail.com>
2024-11-24 00:19:51 -0500

    use readline + history

---
+++ include/main.h
@@ -0,0 +1,17 @@
+// include/main.h
+#ifndef MAIN_H
+#define MAIN_H
+#include <array>
+#include <string_view>
+
+using namespace std;
+
+#ifdef _WIN32
+/* Don't know what the windows equivalent of ~/.config/ is */
+constexpr array<string_view, 2> config_dir = {".config", "lmk"};
+#else
+constexpr array<string_view, 2> config_dir = {".config", "lmk"};
+#endif
+constexpr string_view histfile = ".lmkhistory";
+
+#endif /* MAIN_H */
