d59e5f2ba1ba98253adb8ea8671914e07bcab9f0
Kristófer Fannar Björnsson <kristoferfannarb@gmail.com>
2024-11-27 16:45:09 -0500

    setup first tests

--- test/main.cpp
+++ test/main.cpp
@@ -1,6 +1,16 @@
 #include <gtest/gtest.h>
 #include <iostream>
 
-int main() {
+std::string hw() {
+    return "hello world";
+}
+
+TEST(HelloWorldTest, saysHelloWorld) {
+    ASSERT_EQ(hw(), "hello world");
+}
+
+int main(int argc, char *argv[]) {
     std::cout << "testing\n";
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
