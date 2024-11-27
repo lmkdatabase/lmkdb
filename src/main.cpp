87583f9d698be9cfe8e22bcc368bf1609009fc10
Lsig <logis21@ru.is>
2024-11-27 16:32:33 -0500

    fix: Use namespace fs in main

--- src/main.cpp
+++ src/main.cpp
@@ -6,37 +6,38 @@
 #include <iostream>
 #include "Interpreter.h"
 
+namespace fs = boost::filesystem;
 using namespace std;
 
-boost::filesystem::path get_or_create_cfg_path() {
+fs::path get_or_create_cfg_path() {
 #ifdef _WIN32
     const std::string home(getenv("USERPROFILE"));
 #else
     const string home(getenv("HOME"));
 #endif
-    boost::filesystem::path cfg_path = home;
+    fs::path cfg_path = home;
 
     for (auto path : config_dir) {
         cfg_path /= path; /* boost syntax for path appends */
     }
 
-    if (!boost::filesystem::exists(cfg_path)) {
+    if (!fs::exists(cfg_path)) {
         cout << "initializing cfg dir at " << cfg_path.string() << "\n";
-        boost::filesystem::create_directory(cfg_path);
+        fs::create_directory(cfg_path);
     }
 
     return cfg_path;
 }
 
 void init_lmk() {
-    boost::filesystem::path cfg_path = get_or_create_cfg_path();
-    boost::filesystem::path histpath = cfg_path / histfile;
+    fs::path cfg_path = get_or_create_cfg_path();
+    fs::path histpath = cfg_path / histfile;
     read_history(histpath.c_str());
 }
 
 void exit_lmk() {
-    boost::filesystem::path cfg_path = get_or_create_cfg_path();
-    boost::filesystem::path histpath = cfg_path / histfile;
+    fs::path cfg_path = get_or_create_cfg_path();
+    fs::path histpath = cfg_path / histfile;
     write_history(histpath.c_str());
 }
 
